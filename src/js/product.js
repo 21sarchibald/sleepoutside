d13724da97680eb0b97f799394320f017ad1a9df
Sophie Archibald <arc21004@byui.edu>
2025-09-24 18:54:34 -0600

    Fixed addProductToCart function

--- src/js/product.js
+++ src/js/product.js
@@ -1,9 +1,15 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // Retrieve existing cart and set it equal to cartItems
+  let cartItems = getLocalStorage("so-cart");
+  // Add new product to existing cart
+  cartItems.push(product);
+  // Store the updated cart in local storage
+  setLocalStorage("so-cart", cartItems);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await findProductById(e.target.dataset.id);
